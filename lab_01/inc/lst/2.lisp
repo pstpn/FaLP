06dbbd79be565d6853e21ca4b15991a01207e4dc
31steets31 <stepaha78@gmail.com>
2024-02-19 08:12:51 +0300

    adding report

---
+++ lab_01/inc/lst/2.lisp
@@ -0,0 +1,8 @@
+(car (cdr '(A B C D)))             ; Второй элемент списка
+(cadr '(A B C D))
+
+(car (cdr (cdr '(A B C D))))       ; Третий элемент списка
+(caddr '(A B C D))
+
+(car (cdr (cdr (cdr '(A B C D))))) ; Четвертый элемент списка
+(cadddr '(A B C D))
